76a9807eec68dbb27084498c183067a60526de0e
Shaun-Roy <146830840+Shaun-Roy@users.noreply.github.com>
2025-01-04 09:23:37 +0530

    fixed typo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 
-GOOGLE_API_KEY = "enter code here"
+GOOGLE_API_KEY = "enter key here"
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-pro')
 
